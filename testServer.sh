acf163b6e5e9068b332a2c7ded3ea750147d4435
arkaydias <kevin.sullivan016@gmail.com>
2019-11-21 11:00:22 -0500

    part 1 complete

---
+++ testServer.sh
@@ -0,0 +1,13 @@
+set -x
+
+echo ls > /tmp/server-kevinsullivan-fifo
+
+echo pwd > /tmp/server-kevinsullivan-fifo
+
+echo ls -l > /tmp/server-kevinsullivan-fifo
+
+echo echo "Hello" > /tmp/server-kevinsullivan-fifo
+
+echo status > /tmp/server-kevinsullivan-fifo
+
+echo shutdown > /tmp/server-kevinsullivan-fifo
